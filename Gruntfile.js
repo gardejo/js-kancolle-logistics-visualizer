80f1357303e5237d6ff5b75d9486f989a984f861
MORIYA Masaki (gardejo) <moriya@ermitejo.com>
2014-08-01 23:03:48 +0900

    Fragment a monolithic script file

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,8 +39,70 @@ module.exports = function(grunt) {
                 '!chart/.exists'
             ],
             dist: [
+                '<%= concat.dist.dest %>',
                 '*.zip'
             ]
+        },
+        concat: {
+            options: {
+                stripBanners: true,
+                process: function(source, path) {
+                    return source.replace(
+                        // Remove script mode syntax and append origin.
+                        /(^|\r?\n)\s*('use strict'|"use strict");?\s*/,
+                        [
+                            '// Source: ' + path,
+                            '',
+                            ''
+                        ].join(grunt.util.linefeed)
+                    );
+                },
+                banner: [
+                    '/**',
+                    ' * @fileOverview <%= pkg.description %>.',
+                    ' *     This module added {@code <%= pkg.name %>} ' +
+                        'symbol to the global namespace.',
+                    ' * @version <%= pkg.version %> (Released at ' +
+                        '<%= grunt.template.today("isoDateTime") %>' +
+                        '<%= grunt.template.today("o") %>)',
+                    ' * @author <%= pkg.author.email %> ' +
+                        '(<%= pkg.author.name %>)',
+                    ' * @license <%= pkg.licenses[0].type %> ' +
+                        '(See LICENSE file)',
+                    ' */',
+                    '',
+                    "'use strict'; // Script mode syntax (Whole-library)",
+                    '',
+                    ''
+                ].join(grunt.util.linefeed)
+            },
+            dist: {
+                src: [
+                    // Note: Don't specify 'lib/*.js'! It is order-sensitive.
+                    'lib/main.js', // It has script mode and global namespace.
+                    'lib/inscription.js',
+                    'lib/factory.js',
+                    'lib/formatter.js',
+                    'lib/exception.js',
+                    'lib/array.js',
+                    'lib/pseudo-interface.js',
+                    'lib/configuration.js',
+                    'lib/stream.js',
+                    'lib/date.js',
+                    'lib/game.js',
+                    'lib/agent.js',
+                    'lib/tokenizer.js',
+                    'lib/selector.js',
+                    'lib/projector.js',
+                    'lib/relation.js',
+                    'lib/table.js',
+                    'lib/chart.js',
+                    'lib/template.js',
+                    'lib/visualizer.js'
+                ],
+                dest: 'lib/kclv.js',
+                nonull: true
+            }
         },
         compress: {
             dist: {
@@ -55,7 +117,8 @@ module.exports = function(grunt) {
                             'configuration.json',
                             'visualizer.wsf',
                             'chart/**',
-                            'lib/**',
+                            '<%= concat.dist.dest %>',
+                            'lib/polyfill.js',
                             'template/**'
                         ]
                     }
@@ -65,7 +128,9 @@ module.exports = function(grunt) {
         jshint: {
             files: [
                 // 'configuration.json',
-                'lib/kclv.js',
+                'lib/*.js',
+                '!<%= concat.dist.dest %>',
+                '!lib/polyfill.js',
                 'test/*.js',
                 'test/.jshintrc'
             ],
@@ -109,12 +174,14 @@ module.exports = function(grunt) {
     );
 
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.registerTask(
         'dist',
         [
             'clean',
             'markdown',
+            'concat',
             'compress:dist'
         ]
     );
@@ -127,6 +194,7 @@ module.exports = function(grunt) {
         [
             'csslint',
             'jshint',
+            'concat',
             'qunit'
         ]
     );
