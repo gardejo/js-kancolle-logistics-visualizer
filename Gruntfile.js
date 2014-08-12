3be23ddbc568318b1fcc79c85e9c82016636b64c
MORIYA Masaki (gardejo) <moriya@ermitejo.com>
2014-08-12 23:53:50 +0900

    Modify making of a distribution to clarify compress files

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,7 +138,7 @@ module.exports = function(grunt) {
                             'LICENSE',
                             'configuration.json',
                             'visualizer.wsf',
-                            '*.csv',
+                            '*-sample.csv',
                             'chart/**',
                             '<%= concat.dist.dest %>',
                             'lib/polyfill.js',
